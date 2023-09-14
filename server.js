009884cd7bbedda94993ba46e0c3b49e0de08250
Son Giang <sgiangt@gmail.com>
2023-09-14 17:24:58 -0600

    added middleware and get functions

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware for handling JSON data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET /api/notes
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+  res.json(notes);
+});
+
